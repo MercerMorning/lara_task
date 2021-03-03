5f9d473b0e09d1e7050f4cb34910472c9df09af6
MercerMorning <hugopochta@gmail.com>
2021-03-03 18:17:41 +0300

    commit

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,7 +9,7 @@ class CategoryController extends Controller
 {
     public function show(Category $category)
     {
-        return view('home', ['cheapestProducts' => $category->products()]);
+        return view('home', ['cheapestProducts' => $category->products]);
 
     }
 }
